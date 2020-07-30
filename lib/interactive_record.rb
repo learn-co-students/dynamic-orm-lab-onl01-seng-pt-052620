32ca8cd5922a7ab1082e8ea6551a57255db6fed6
Ronald Burrell Jr <ronburrell05@gmail.com>
2020-07-30 15:52:38 +0000

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -7,7 +7,7 @@ class InteractiveRecord
   end 
   
   def self.column_names
-    DB[:conn].results_as_hash = true
+    #DB[:conn].results_as_hash = true
     
     sql = "PRAGMA table_info ('#{table_name}')"
     
@@ -47,16 +47,17 @@ class InteractiveRecord
   end
   
   def save
-    DB[:conn].execute("INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (?)", [values_for_insert])
+    DB[:conn].execute("INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})")
     
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
   end
   
-  def find_by_name(name)
-    DB[:conn].execute("SELECT * FROM #{table_name_for_insert} WHERE name = ?", [name])
+  
+  def self.find_by_name(name)
+    DB[:conn].execute("SELECT * FROM #{table_name} WHERE name = ?", [name])
   end
   
-  def find_by(attribute)
+  def self.find_by(attribute)
     column = attribute.keys[0]
     value = attribute.values[0]
     
@@ -65,6 +66,6 @@ class InteractiveRecord
       WHERE #{column} = ?
     SQL
     
-    
+    DB[:conn].execute(sql, value)
   end
 end
