6697df2d1787becc11fc99a220e93a4b8ebb2e31
pete3249 <katelynrpeterson16@gmail.com>
2020-07-31 16:03:53 -0400

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -1,6 +1,60 @@
 require_relative "../config/environment.rb"
 require 'active_support/inflector'
+require 'pry'
 
 class InteractiveRecord
+
+    def initialize(options = {})
+        options.each do |property, value|
+            self.send("#{property}", value)
+        end 
+    end 
+    
+    def self.table_name
+        self.to_s.downcase.pluralize
+    end 
+
+    def self.column_names
+        DB[:conn].results_as_hash = true
+
+        sql = "PRAGMA table_info('#{table_name}')"
+
+        table_info = DB[:conn].execute(sql)
+        column_names = []
+
+        table_info.each do |column|
+            column_names << column["name"]
+        end 
+
+        column_names.compact
+    end 
+
+    self.column_names.each do |col_name|
+        attr_accessor col_name.to_sym
+    end 
+
+    def table_name_for_insert
+        self.class.table_name
+    end 
+
+    def col_name_for_insert
+        self.class.column_names.delete_if {|col_name| col_name == "id"}.join(",")
+    end 
+
+    def values_for_insert
+        values = []
+        self.column_names.each do |col_name|
+            values << "'#{send(col_name)}'" unless send(col_name).nil?
+        end 
+        values.join(",")
+    end
+
+    # def save
+    #     DB[:conn]
+    # end 
   
 end
+
+    # it 'sets the student\'s id' do
+    #   new_student.save
+    #   expect(new_student.id).to eq(1)
