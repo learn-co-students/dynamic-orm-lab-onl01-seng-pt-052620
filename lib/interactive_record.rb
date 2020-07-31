917866bb7f2ba9a64d27ce1c7f9802a3ce54a361
pete3249 <katelynrpeterson16@gmail.com>
2020-07-31 16:23:48 -0400

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -29,10 +29,6 @@ class InteractiveRecord
         column_names.compact
     end 
 
-    self.column_names.each do |col_name|
-        attr_accessor col_name.to_sym
-    end 
-
     def table_name_for_insert
         self.class.table_name
     end 
